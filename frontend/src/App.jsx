3fec5bcecb5eb054067ecf46ff528617ecb18a1e
Sara Hurtado <shurtadom3@eafit.edu.co>
2026-05-27 16:35:36 -0500

    style(frontend): actualizar estilos y punto de entrada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,24 @@ import { Routes, Route } from "react-router-dom";
 import Index from "./components/index";
 import Products from "./pages/products";
 import Combos from "./pages/combo";
+import Login from "./pages/login";
+import Guarantees from "./pages/guarantees";
+import CartSidebar from "./components/CartSidebar";
 
 
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/combo" element={<Combos/>} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/combo" element={<Combos/>} />
+        <Route path="/garantias" element={<Guarantees />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <CartSidebar />
+    </>
 
   )
 }
