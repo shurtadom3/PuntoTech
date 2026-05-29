f6623251a91752eb7a7fb14572fbb1f340e9af25
mari <msancheza8@eafit.edu.co>
2026-05-28 23:29:34 -0500

    feat(frontend): renovar catalogo de productos

--- frontend/src/components/products.tsx
+++ frontend/src/components/products.tsx
@@ -15,3 +15,4 @@ const ProductsComponent = () => {
 };
 
 export default ProductsComponent;
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './context/CartContext'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </BrowserRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
